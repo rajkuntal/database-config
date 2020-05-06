b223d17e716c2c82210b0a021154255290a786da
Raj Kumar <raj.kumar@1stdibs.com>
2020-05-06 16:39:45 +0530

    new column added

--- dynamilc_table.sql
+++ dynamilc_table.sql
@@ -1,5 +1,6 @@
 CREATE TABLE `dynamilc_table` (
   `id` int(11) DEFAULT NULL,
   `name` int(11) DEFAULT NULL,
-  `total` int(11) DEFAULT NULL
+  `total` int(11) DEFAULT NULL,
+  `new_test` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
