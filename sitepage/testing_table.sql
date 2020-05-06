8ce2dc42197b15acaca9cd090328121d452226c8
Raj Kumar <raj.kumar@1stdibs.com>
2020-05-06 17:42:49 +0530

    adding primary key

--- sitepage/testing_table.sql
+++ sitepage/testing_table.sql
@@ -1,4 +1,5 @@
 CREATE TABLE `testing_table` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `url` varchar(500) COLLATE utf8mb4_unicode_ci NOT NULL
+  `url` varchar(500) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
   );
