54e676e81832a8a2c50363c4a8bcc1791e3ad7c7
Raj Kumar <raj.kumar@1stdibs.com>
2020-05-14 17:50:24 +0530

    more changes for testing

--- scripts/dml_changes.sh
+++ scripts/dml_changes.sh
@@ -1,9 +1,15 @@
-if [ -s /tmp/skeema-ci/skeema-diff.sql ] ; then
-	sed -i "" 's/-- instance: 127.0.0.1:3306/-- skeema-diff-comment \
-	\
-	```sql \
-	-- ddl queries /g' /tmp/skeema-ci/skeema-diff.sql
-	touch /tmp/skeema-ci/skeema-diff-hint.hint
-else
-	echo $'-- skeema-diff-comment \n\n ```sql ' >> /tmp/skeema-ci/skeema-diff.sql
-fi
+if [ -f /tmp/skeema-ci/skeema-diff-exists-hint.hint ] ; then
+          # There is a schema change
+          if [ -f /tmp/skeema-ci/skeema-diff-label-detected.hint ] ; then
+            curl -X POST -u rajkuntal:798uoonsdfnjkghkj \
+              "https://api.github.com/repos/rajkuntal/database-config/issues/11/labels" \
+              -H "Content-type: application/json" -H "Accept: application/json" -d '["skeema-diff"]'
+            touch /tmp/skeema-ci/skeema-diff-label-created.hint
+          fi
+        else
+          if [ ! -f /tmp/skeema-ci/skeema-diff-label-detected.hint ] ; then
+            # remove existing label
+            /tmp/skeema-ci/hub api --method DELETE "/repos/rajkuntal/database-config/issues/${PR_NUMBER}/labels/skeema-diff" || :
+            touch /tmp/skeema-ci/skeema-diff-label-removed.hint
+          fi
+        fi
