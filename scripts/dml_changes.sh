8feec6727948adc960f1b5e55f1db783c412d1a9
Raj Kumar <raj.kumar@1stdibs.com>
2020-05-14 15:17:53 +0530

    more changes

--- scripts/dml_changes.sh
+++ scripts/dml_changes.sh
@@ -1,14 +1,9 @@
-git diff master..test_pr_2 --name-only | tee /tmp/skeema-ci/dml-changes.txt
-dmQueries="DML_Queries"
-if grep -q "$dmQueries" /tmp/skeema-ci/dml-changes.txt; then
-	echo '' >> /tmp/skeema-ci/skeema-diff.sql
-	echo '' >> /tmp/skeema-ci/skeema-diff.sql
-	echo '-- DML Queries' >> /tmp/skeema-ci/skeema-diff.sql
+if [ -s /tmp/skeema-ci/skeema-diff.sql ] ; then
+	sed -i "" 's/-- instance: 127.0.0.1:3306/-- skeema-diff-comment \
+	\
+	```sql \
+	-- ddl queries /g' /tmp/skeema-ci/skeema-diff.sql
+	touch /tmp/skeema-ci/skeema-diff-hint.hint
+else
+	echo '-- skeema-diff-comment \n\n ```sql ' >> /tmp/skeema-ci/skeema-diff.sql
 fi
-while IFS="" read -r p || [ -n "$p" ]
-do
-  if [[ "$p" == *"DML_Queries"* ]]; then
-  	cp -v "$p" /tmp/skeema-ci/"${p//\//_}"
-  fi
-done < /tmp/skeema-ci/dml-changes.txt
-cat /tmp/skeema-ci/skeema-diff.sql /tmp/skeema-ci/DML_Queries*.sql | tee /tmp/skeema-ci/all_sql_changes.sql
